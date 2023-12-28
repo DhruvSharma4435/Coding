5615d369e3e087aa770143dc0c2dc6efabd452fc
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2023-12-29 01:25:43 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+int perfectSum(int arr[], int n, int sum)
+	{
+	    int m=1e9+7;
+	    int count=0;
+        long long dp[n+1][sum+1];
+        
+        for(int i=0 ; i<n+1 ; i++){
+            for(int j=0 ; j<sum+1 ; j++){
+                if(i==0)dp[i][j]=0;
+                if(j==0)dp[i][j]=1;
+            }
+        }
+        
+        for(int i=1 ; i<n+1 ; i++){
+            for(int j=0 ; j<sum+1 ; j++){
+                if(j>=arr[i-1])dp[i][j]=(dp[i-1][j]+dp[i-1][j-arr[i-1]])%m;
+                else dp[i][j]=dp[i-1][j]%m;
+            }
+        }
+        return dp[n][sum]%m;
+    
+	}
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
