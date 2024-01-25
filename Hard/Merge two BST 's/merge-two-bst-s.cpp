ee32bc37b5304c6da700263e8e1a8a53ee92e595
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-01-25 23:32:57 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Merge two BST 's/merge-two-bst-s.cpp
@@ -0,0 +1,188 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+
+
+// } Driver Code Ends
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution
+{
+    public:
+    // use preOrder travesal
+    void findArray(Node *root, vector<int>&ans)
+    {
+        if(!root)
+        return ;
+        
+        findArray(root->left, ans);
+        
+        ans.push_back(root->data);
+        
+        findArray(root->right, ans);
+        
+    }
+    
+    
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    vector<int> merge(Node *root1, Node *root2)
+    {
+        // calculate root1 array
+        vector<int>ans1;
+        // calculate roo2 array
+        vector<int>ans2;
+        
+        findArray(root1, ans1);
+        findArray(root2, ans2);
+        
+        vector<int>ans;
+        int first = 0, second = 0;
+        
+        // marge both array in sorted order
+        while(ans1.size() != first && ans2.size() != second)
+        {
+            if(ans1[first]<ans2[second])
+            {
+                ans.push_back(ans1[first++]);
+            }
+            else
+            {
+                ans.push_back(ans2[second++]);
+            }
+        }
+        while(ans1.size() !=first)
+        {
+            ans.push_back(ans1[first++]);
+        }
+        while(ans2.size() != second)
+        {
+            ans.push_back(ans2[second++]);
+        }
+        
+        
+        return ans;
+        
+       //Your code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root1 = buildTree(s);
+
+       getline(cin, s);
+       Node* root2 = buildTree(s);
+    
+       // getline(cin, s);
+       Solution obj;
+       vector<int> vec = obj.merge(root1, root2);
+       for(int i=0;i<vec.size();i++)
+            cout << vec[i] << " ";
+        cout << endl;
+       ///cout<<"~"<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
