864152fe91779d6e55c39910962fbcf58b867226
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-06 10:33:39 +0530

    Time: 0 ms (100%), Space: 9.7 MB (10.06%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-   int splitArray(vector<int>& nums, int m) {
-    const int n = nums.size();
-    // dp[i][k] := min of largest sum to split first i nums into k groups
-    dp.resize(n + 1, vector<int>(m + 1, INT_MAX));
-    prefix.resize(n + 1);
+    bool isPossible(vector<int> nums, int mid, int k){
+        int sum = 0;
+        int count = 1;
+        for(int i = 0; i< nums.size(); i++){
+            if((sum + nums[i])<= mid)
+               sum += nums[i];
+            else{
+                count++;
+                if(count> k || nums[i]> mid)
+                  return false;
+                sum = nums[i];
+            }
+        }
+        return true;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        //first we create the sample space
+        int sum  = 0;
+        for(int i = 0; i<nums.size(); i++)
+           sum += nums[i];
+        int s = 0, e = sum, ans = -1;
+        int mid = s + (e-s)/2;
+        while(s <= e){
+            if(isPossible(nums, mid, k)){
+                ans = mid;
+                e = mid-1;
+            }
+            else 
+                s = mid+ 1;
 
-    partial_sum(nums.begin(), nums.end(), prefix.begin() + 1);
-    return splitArray(nums, n, m);
-  }
-
- private:
-  vector<vector<int>> dp;
-  vector<int> prefix;
-
-  int splitArray(const vector<int>& nums, int i, int k) {
-    if (k == 1)
-      return prefix[i];
-    if (dp[i][k] < INT_MAX)
-      return dp[i][k];
-
-    // Try all the possible partitions.
-    for (int j = k - 1; j < i; ++j)
-      dp[i][k] =
-          min(dp[i][k], max(splitArray(nums, j, k - 1), prefix[i] - prefix[j]));
-
-    return dp[i][k];
-  }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
 };
