c9c22b0ec70ebd528973e5bdb3bea713fb05a41b
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-13 12:16:12 +0530

    Time: 0 ms (100%), Space: 7.7 MB (93.94%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    string removeIndices(string str, int indexFrom, int indexTo)
-    {   int n = str.length();
-        int size = str.length(), k = indexTo - indexFrom + 1;
-        for(int j = indexFrom + k; j< size; j++)
-        {
-            str[j - k] = str[j];
-        }
-        str.resize(n - k);
-        return str;
-    }
     string removeOccurrences(string s, string part) {
-        
-        while(s.find(part) != -1)
+        int n = part.size();
+        int i = 0; 
+        while(1)
         {
-        int indexFrom = s.find(part);
-        int indexTo = indexFrom +  part.length() -1;
-         s = removeIndices(s, indexFrom, indexTo);
+           i =  s.find(part);
+           if(i == -1)
+             break;
+
+           s.erase(i, n);
         }
         return s;
     }
