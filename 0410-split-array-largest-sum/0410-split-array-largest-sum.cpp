ca4b271dfea3941274ffd6a13d3ae20a0f0dff8e
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-14 11:48:12 +0530

    Time: 2 ms (52.08%), Space: 9.5 MB (13.22%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
-    bool isPossible(vector<int> nums, int mid, int k){
-        int sum = 0;
-        int count = 1;
-        for(int i = 0; i< nums.size(); i++){
-            if((sum + nums[i])<= mid)
+    bool isValid(vector<int> nums, int mid, int k )
+    {   
+        int sum = 0, count = 1;
+        for(int i = 0; i< nums.size(); i++)
+        {
+            if(sum + nums[i] <= mid)
                sum += nums[i];
-            else{
+            else
+            {
                 count++;
-                if(count> k || nums[i]> mid)
+                if(count>k ||  nums[i] > mid)
                   return false;
+                
                 sum = nums[i];
             }
         }
-        return true;
+        return true; 
     }
-    int splitArray(vector<int>& nums, int k) {
-        //first we create the sample space
-        int sum  = 0;
-        for(int i = 0; i<nums.size(); i++)
-           sum += nums[i];
-        int s = 0, e = sum, ans = -1;
-        int mid = s + (e-s)/2;
-        while(s <= e){
-            if(isPossible(nums, mid, k)){
-                ans = mid;
-                e = mid-1;
-            }
-            else 
-                s = mid+ 1;
 
-            mid = s + (e-s)/2;
+    //already solved question hai, 1 st revision kr raha hu
+    int splitArray(vector<int>& nums, int k) {
+        int sum = 0;
+        for(int i = 0; i< nums.size(); i++)
+           sum += nums[i];
+        //Sample space to bana li
+
+        int s = 0, e = sum;
+        int mid = s + (e -s)/2;
+        int ans = -1;
+        while(s <= e)
+        {
+            if(isValid(nums,mid, k))
+              {
+                ans = mid;
+                e = mid - 1;
+              }
+            else 
+               s = mid + 1;
+            mid = s + ( e -s)/2;
         }
+
         return ans;
     }
 };
