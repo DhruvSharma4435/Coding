799d29b986831f6beee9552799e976c79e491ab6
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-15 17:23:12 +0530

    Time: 36 ms (93.99%), Space: 17.2 MB (86.41%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,47 +1,18 @@
 class Solution {
 public:
-   bool binarySearchRow(vector<vector<int>>& matrix, int row, int target) {
-        int left = 0, right = matrix[row].size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (matrix[row][mid] == target) {
-                return true;
-            } else if (matrix[row][mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return false;
-    }
-    
-    bool binarySearchColumn(vector<vector<int>>& matrix, int col, int target) {
-        int top = 0, bottom = matrix.size() - 1;
-        while (top <= bottom) {
-            int mid = top + (bottom - top) / 2;
-            if (matrix[mid][col] == target) {
-                return true;
-            } else if (matrix[mid][col] < target) {
-                top = mid + 1;
-            } else {
-                bottom = mid - 1;
-            }
-        }
-        return false;
-    }
-    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        
-        // Iterate over the diagonals
-        for (int i = 0; i < std::min(rows, cols); ++i) {
-            // Perform binary search in the row and column starting from (i, i)
-            if (binarySearchRow(matrix, i, target) || binarySearchColumn(matrix, i, target)) {
-                return true;
-            }
+        int row = matrix.size(), col = matrix[0].size();
+        int i = 0, j = col - 1;
+
+        while(i< row && j>=0)
+        {
+            if(matrix[i][j] == target)
+              return true;
+            else if(matrix[i][j]> target)
+              j--;
+            else
+              i++;
         }
-        
         return false;
     }
 };
