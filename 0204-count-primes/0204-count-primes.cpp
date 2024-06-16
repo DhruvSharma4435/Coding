f62d1c714f5fb9c816a2c9cbd014cc2a7af73785
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-16 06:35:36 +0530

    Time: 525 ms (18.22%), Space: 11.5 MB (69.8%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count = 0; 
+        if(n<= 2)
+          return 0;
+        //sieve therorem se kar rahe hai.
+        //initially mano ki saare numbers prime hai
+        vector<bool> prime(n + 1, true);
+        int i = 2;
+        while(i<n)
+        {   
+            if(prime[i])
+            {
+                count++;
+                for(int j = i*2; j<=n; j += i)
+                   prime[j] = false;
+            }
+            i++;
+        }
+        return count;
+    }
+};
