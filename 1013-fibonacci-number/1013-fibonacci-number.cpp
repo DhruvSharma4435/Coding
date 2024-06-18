6b911d7ac3d21374c33d48428cfe46eaf7c1c417
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-18 14:25:19 +0530

    Time: 3 ms (66.56%), Space: 7.4 MB (11.74%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
+    ios::sync_with_stdio(0);
     double phi = (1 + sqrt(5)) / 2;
     double psi = (1 - sqrt(5)) / 2;
     return round((pow(phi, n) - pow(psi, n)) / sqrt(5));
