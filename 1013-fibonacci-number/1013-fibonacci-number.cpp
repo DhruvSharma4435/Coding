8fd2aa3be1a115ee02aa96bd9ec4f1e4ac6e1d1e
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-18 14:24:39 +0530

    Time: 0 ms (100%), Space: 7.2 MB (11.74%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        ios::sync_with_stdio(0);
-        if(n == 0)
-           return 0;
-        if(n == 1)
-           return 1;
-           
-        int prev1 = 1, prev2 = 0, ans = 0;
-        for(int i = 2; i<= n; i++)
-        {
-            ans = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = ans;
-        }
-        return ans;
+    double phi = (1 + sqrt(5)) / 2;
+    double psi = (1 - sqrt(5)) / 2;
+    return round((pow(phi, n) - pow(psi, n)) / sqrt(5));
     }
 };
