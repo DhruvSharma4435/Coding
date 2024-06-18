66674a3de0d98b847cc4ac5fea21251637e34320
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-18 14:51:09 +0530

    Time: 0 ms (100%), Space: 7.1 MB (50.24%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        ios::sync_with_stdio(0);
         if(n == 0)
           return 0;
         if(n == 1 || n == 2)
           return 1;
-        int prev1 = 1, prev2 = 1, prev3 = 0, ans = 0;
-        for(int i = 3; i<= n ; i++)
+        vector<int> arr(38);
+        arr[0] = 0;
+        arr[1] = arr[2] = 1;
+        int ans = 0;
+        for(int i = 3; i<= n; i++)
         {
-            ans = prev1 + prev2 + prev3;
-            prev3 = prev2;
-            prev2 = prev1;
-            prev1 = ans;
+            arr[i] = arr[i-3] + arr[i - 2] + arr[i - 1];
+            ans = arr[i];
         }
-        return prev1;
-
+        return ans;
     }
 };
