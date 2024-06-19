611a0b301ed816fea7df9f0b28cc40d5f4a7ea99
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-19 15:50:06 +0530

    Time: 4 ms (65.17%), Space: 16.1 MB (49.43%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //pahele binary search indexes ke upar laga ke minIndex nikaal le
+        int s = 0, e = nums.size() - 1, minIndex = INT_MAX, maxIndex = -1;
+        int mid = s + (e -s)/2;
+        while(s <= e)
+        {
+            if(nums[mid] == target)
+            {
+                minIndex = min(minIndex, mid);
+                e = mid - 1;
+            }
+            if(nums[mid] < target)
+               s = mid + 1;
+            else 
+               e = mid - 1;
+            mid = s + (e -s)/2;
+        }
+        s = 0; e = nums.size() - 1;
+        mid = s + (e -s)/2;
+
+        //same kaam maxIndex ke liye
+        while(s <= e)
+        {
+            // cout<<"mid = "<<mid;
+            if(nums[mid] == target)
+            {
+                maxIndex = max(maxIndex, mid);
+                s = mid + 1;
+            }
+            else if(nums[mid] < target)
+               s = mid + 1;
+            else 
+               e = mid - 1;
+            mid = s + (e -s)/2;
+            cout<<"mid = "<<mid;
+        }
+        vector<int> ans;
+        if(minIndex == INT_MAX)
+            minIndex = -1;
+        ans.push_back(minIndex);
+        ans.push_back(maxIndex);
+        return ans;
+    }
+};
