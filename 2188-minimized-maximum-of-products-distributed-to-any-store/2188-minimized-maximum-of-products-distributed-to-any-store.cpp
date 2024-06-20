e2e5748a5e9d48abab28085cee28ab84cf8e34fa
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-06-20 09:46:03 +0530

    Time: 225 ms (7.76%), Space: 86.8 MB (64.86%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+  bool isPossible(const std::vector<int>& quantities, int mid, int n) {
+        int count = 0;
+        
+        for (int i = 0; i < quantities.size(); ++i) {
+            long long quantity = quantities[i];
+            // Calculate how many stores are needed for the current quantity
+            while (quantity > 0) {
+                quantity -= mid;
+                count++;
+                if (count > n) {
+                    return false;
+                }
+            }
+        }
+        return count <= n;
+    }
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        //sabse pahel sample sapce bana le, aur phir use BS se conquer kr le
+        long long sum = 0;
+        for(auto i : quantities)
+           sum += i;
+
+        long long  s = 1, e = sum;
+        long long mid = s + (e - s)/2, ans = -1;
+        while(s<=e)
+        {
+            if(isPossible(quantities, mid, n)){
+                ans = mid;
+                e = mid - 1;
+            }
+            else
+                s = mid + 1;
+            mid = s + (e - s)/2;
+        }
+        return mid;
+    }
+};
