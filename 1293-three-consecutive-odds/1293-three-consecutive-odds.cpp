78c700583cdfecee850f7f2980e3c1779e198731
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-07-01 23:33:27 +0530

    Time: 0 ms (100%), Space: 10.5 MB (45.76%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
+    //little better solution this time maybe...
     bool threeConsecutiveOdds(vector<int>& arr) {
-        ios::sync_with_stdio(0);
+        
         if(arr.size() < 3)
           return false;
-        //create intitial window
-        bool first = false, second = false, third = false;
-        if(arr[0] & 1)
-           first = true;
-        if(arr[1] & 1)
-           second = true;
-        if(arr[2] & 1)
-           third = true;   
-        if(first == true  &&  second == true  &&  third == true)
-           return true;
-        
-        for(int j = 3; j < arr.size(); j++)
-        {
-            first = second;
-            second = third;
-            if(arr[j] & 1)
-              third = true;
-            else 
-              third = false;
-
-            if(first == true  &&  second == true  &&  third == true)
-               return true;  
-        }
+        for(int i = 0; i < arr.size() - 2; i++) {
+        if(arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0)
+            return true;
+    }
         return false;
          
     }
