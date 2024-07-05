caf066a1179a759b359fb308f0c93a5ffd543a7f
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-07-05 13:48:20 +0530

    Time: 125 ms (97.73%), Space: 119.7 MB (35.95%) - LeetHub

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,89 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    // bool isMinima(ListNode * curr, ListNode * prev, ListNode * forward)
+    // {
+    //     if(prev == NULL || forward == NULL)
+    //        return false;
+    //     if(curr -> val < prev -> val  &&  curr-> val < forward -> val)
+    //        return true;
+    //     else return false;
+    // }
+
+    // bool isMaxima(ListNode * curr, ListNode * prev, ListNode * forward)
+    // {
+    //     if(prev == NULL || forward == NULL)
+    //        return false;
+    //     if(curr -> val > prev -> val  &&  curr-> val > forward -> val)
+    //        return true;
+    //     else return false;
+    // }
+    bool isCritical(ListNode * curr, ListNode * prev, ListNode * forward)
+    {
+         if(prev == NULL || forward == NULL)
+              return false;
+         if(curr -> val < prev -> val  &&  curr-> val < forward -> val)
+              return true;
+         if(curr -> val > prev -> val  &&  curr-> val > forward -> val)
+              return true;
+         return false;
+    }
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        ios::sync_with_stdio(0);
+        if(head == NULL || head -> next == NULL || head -> next -> next == NULL)
+           return {-1, -1};
+        ListNode * prev = NULL, *curr = head, *forward = NULL;
+        vector<int> ans(2);
+        int index = 1;
+        vector<int> indices;
+        ListNode * temp = head;
+        while(temp != nullptr)
+        {
+            forward = temp -> next;
+            if(isCritical(temp, prev, forward))
+            {
+                indices.push_back(index);
+            }
+            index++;
+            temp = temp -> next;
+            prev = curr; curr = temp;
+        }
+        for(auto i : indices)
+          cout<<i <<" ";
+        if(indices.empty())
+           return {-1, -1};
+        if(indices.size() == 1)
+           return {-1,-1};
+        ans[1] = indices[indices.size() - 1] - indices[0];
+        int mini = INT_MAX;
+        for(int i = 1; i < indices.size(); i++)
+        {
+            mini = min(mini, indices[i] - indices[i - 1]);
+        }
+        ans[0] = mini;
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
