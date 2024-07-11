d3cb555c68c418f6a447ce9e70fb421950de30d9
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-07-11 17:04:05 +0530

    Time: 15 ms (87.76%), Space: 75.9 MB (93.95%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,72 +11,24 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ios::sync_with_stdio(0);
-        vector<int> arr1, arr2;
-        ListNode* temp1 = l1, *temp2 = l2;
-        while(temp1 != nullptr)
-        {
-            arr1.push_back(temp1 -> val);
-            temp1 = temp1 -> next;
-        }
-        while(temp2 != nullptr)
-        {
-            arr2.push_back(temp2 -> val);
-            temp2 = temp2 -> next;
-        }
-        int i = 0, j = 0;
-        vector<int> arr3;
-        int carry = 0;
-        while(i < arr1.size() && j < arr2.size())
-        {
-            int num = arr1[i] + arr2[j] + carry;
-            if(num <= 9){
-                arr3.push_back(num);
-                carry = 0;
-            }
-            else{
-                arr3.push_back(num%10);
-                carry = 1;
-            }
-            i++; j++;
-        }
-        while( i < arr1.size())
-        {
-            int num = arr1[i] + carry;
-            if(num <= 9){
-                arr3.push_back(num);
-                carry = 0;
-            }
-            else{
-                arr3.push_back(num % 10);
-                carry = 1;
-            }
-            i++;
-        }
-        while( j < arr2.size())
-        {
-            int num = arr2[j] + carry;
-            if(num <= 9){
-                arr3.push_back(num);
-                carry = 0;
-            }
-            else{
-                arr3.push_back(num % 10);
-                carry = 1;
-            }
-            j++;
-        }
-        if(carry >0)  arr3.push_back(carry);
-        // reverse(arr3.begin(), arr3.end());
-        ListNode * dummy = new ListNode(0), *tail = dummy;
-        for(int k = 0; k < arr3.size(); k++)
-        {
-            ListNode * temp = new ListNode(arr3[k]);
-            tail -> next = temp;
-            tail = temp;
-        }
+    ListNode dummy(0);
+    ListNode* curr = &dummy;
+    int carry = 0;
 
-
-        return dummy -> next;
+    while (l1 != nullptr || l2 != nullptr || carry > 0) {
+      if (l1 != nullptr) {
+        carry += l1->val;
+        l1 = l1->next;
+      }
+      if (l2 != nullptr) {
+        carry += l2->val;
+        l2 = l2->next;
+      }
+      curr->next = new ListNode(carry % 10);
+      carry /= 10;
+      curr = curr->next;
     }
+
+    return dummy.next;
+  }
 };
