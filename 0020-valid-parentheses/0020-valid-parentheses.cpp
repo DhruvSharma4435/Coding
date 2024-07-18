a746aa6859b4f7d03dccaf14aba355bee2ab593d
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-07-19 00:04:20 +0530

    Time: 3 ms (41.31%), Space: 7.9 MB (21.06%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        if(s.length() < 2) return false;
+        
+        stack<char> stak;
+        for(int i = 0; i< s.length(); i++){
+            if((s[i] == '(') || (s[i]== '[') || (s[i] == '{')){
+                stak.push(s[i]);
+            }
+            else {
+                if (stak.empty()) return false;
+                
+                char top = stak.top();
+                if ((s[i] == ')' && top == '(') || 
+                    (s[i] == ']' && top == '[') || 
+                    (s[i] == '}' && top == '{')) {
+                    stak.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(stak.empty())
+           return true;
+        return false;
+    }
+};
