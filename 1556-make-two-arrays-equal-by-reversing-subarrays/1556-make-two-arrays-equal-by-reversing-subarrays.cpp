590dc2877ee333cae1adb374751e7070d0201721
DhruvSharma4435 <130392721+DhruvSharma4435@users.noreply.github.com>
2024-08-03 07:41:20 +0530

    Time: 4 ms (92.55%), Space: 17.4 MB (57.4%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        ios::sync_with_stdio(0);
         // if(target.size() == 1) return true;
         sort(target.begin(), target.end());
         sort(arr.begin(), arr.end());
